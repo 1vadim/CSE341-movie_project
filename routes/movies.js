611d41d87a9ff4263eb1572438102376e2a50d18
1vadim <vchemariev@byupathway.edu>
2026-07-26 00:53:35 +0200

    swagger3

--- routes/movies.js
+++ routes/movies.js
@@ -2,21 +2,11 @@ const express = require('express');
 const router = express.Router();
 const moviesController = require('../controllers/movies');
 const { validateId, movieValidationRules } = require('../middleware/validateMovies');
-const { handleAsync } = require('../middleware/errorHandler');
 
-router.get('/', handleAsync(moviesController.getAll));
-
-router.get('/:id', validateId, handleAsync(moviesController.getSingle));
-
-router.post('/', movieValidationRules, handleAsync(moviesController.createSingle));
-
-router.put(
-  '/:id',
-  validateId,
-  movieValidationRules,
-  handleAsync(moviesController.updateSingle)
-);
-
-router.delete('/:id', validateId, handleAsync(moviesController.deleteSingle));
+router.get('/', moviesController.getAll);
+router.get('/:id', validateId, moviesController.getSingle);
+router.post('/', movieValidationRules, moviesController.createSingle);
+router.put('/:id', validateId, movieValidationRules, moviesController.updateSingle);
+router.delete('/:id', validateId, moviesController.deleteSingle);
 
 module.exports = router;
