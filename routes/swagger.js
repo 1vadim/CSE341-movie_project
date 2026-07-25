24a6ba913c9ca8c17655b49eb56d8de44e5b64b3
1vadim <vchemariev@byupathway.edu>
2026-07-26 00:06:15 +0200

    swagger1

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../swagger-output.json");
-router.use("/api-docs", swaggerUi.serve);
-router.get("/api-docs", swaggerUi.setup(swaggerDocument));
+router.use('/api-docs', swaggerUi.serve, (req, res, next) => {
+  swaggerDocument.host = req.get('host');
+  swaggerDocument.schemes =
+    req.secure || req.headers['x-forwarded-proto'] === 'https' ? ['https'] : ['http'];
+
+  swaggerUi.setup(swaggerDocument)(req, res, next);
+});
 
 module.exports = router;
